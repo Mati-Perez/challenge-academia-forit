a230c1238eafee2b553ed42516372f2501ece11e
Matias Perez <matiperez881@gmail.com>
2025-07-26 03:00:38 -0300

    se eliminaron elementos que se dejaron de usar

--- frontend/tasklist/src/main.jsx
+++ frontend/tasklist/src/main.jsx
@@ -2,7 +2,6 @@ import {
   BrowserRouter as Router,
 } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
